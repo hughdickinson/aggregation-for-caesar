6e90ee91525e2c3dc85db0b5ac44274bb1cfb2b9
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2026-04-17 15:46:23 +0100

    Fixing bug in reducer

--- panoptes_aggregation/reducers/spectrum_mask_count_reducer.py
+++ panoptes_aggregation/reducers/spectrum_mask_count_reducer.py
@@ -11,6 +11,7 @@ def extract_bbox_ids(extract):
         if "version" in frame:
             continue
         bbox_ids.extend(frame_data["bbox_id"])
+        print(bbox_ids)
     return bbox_ids
 
 
@@ -20,9 +21,11 @@ def spectrum_mask_count_reducer(data_list, **kwargs):
     bbox_ids = []
     for extract in data_list:
         bbox_ids.extend(extract_bbox_ids(extract))
+        print(bbox_ids)
 
-    groups = list(itertools.groupby(bbox_ids))
-    reductions["bbox_num_masks"] = list(map(len, groups))
-    reductions["bbox_keys"] = list(map(lambda x: x[0], groups))
+    reductions["bbox_num_masks"] = [
+        len(list(g[1])) for g in itertools.groupby(bbox_ids)
+    ]
+    reductions["bbox_keys"] = [g[0] for g in itertools.groupby(bbox_ids)]
 
     return reductions
